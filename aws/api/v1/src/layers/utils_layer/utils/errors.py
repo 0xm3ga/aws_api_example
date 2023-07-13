fa633f93b5e0b05dba3e0c8503b8aca333bbb679
front-end-guy-2020 <56974140+0xm3ga@users.noreply.github.com>
2023-07-13 22:41:18 +0100

    Added intgration tests

---
+++ aws/api/v1/src/layers/utils_layer/utils/errors.py
@@ -0,0 +1,73 @@
+import http
+
+REQUIRED_NAME_ERROR = "'name' query string parameter is required"
+INVALID_NAME_ERROR = "Invalid name provided"
+DEFAULT_HTTP_ERROR = "An error occurred processing your request."
+UNEXPECTED_HTTP_ERROR = "An unexpected error occurred while processing the response."
+JSON_SERIALIZING_ERROR = "Error serializing response body to JSON: {}"
+
+
+class CustomError(Exception):
+    """Base class for other custom exceptions."""
+
+    status_code = http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+
+class BadRequestError(CustomError):
+    """Custom error for bad requests (400)."""
+
+    status_code = http.HTTPStatus.BAD_REQUEST
+
+
+class MissingRequiredQueryStringParameterError(BadRequestError):
+    """Raised when the required query string parameter is missing"""
+
+    pass
+
+
+class InvalidQueryStringParameterError(BadRequestError):
+    """Raised when the query string parameter is invalid"""
+
+    pass
+
+
+class UnauthorizedError(CustomError):
+    """Custom error for unauthorized requests (401)."""
+
+    status_code = http.HTTPStatus.UNAUTHORIZED
+
+
+class ForbiddenError(CustomError):
+    """Custom error for forbidden requests (403)."""
+
+    status_code = http.HTTPStatus.FORBIDDEN
+
+
+class NotFoundError(CustomError):
+    """Custom error for not found requests (404)."""
+
+    status_code = http.HTTPStatus.NOT_FOUND
+
+
+class InternalServerError(CustomError):
+    """Custom error for internal server errors (500)."""
+
+    status_code = http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+
+class NotImplementedError(CustomError):
+    """Custom error for not implemented errors (501)."""
+
+    status_code = http.HTTPStatus.NOT_IMPLEMENTED
+
+
+class ServiceUnavailableError(CustomError):
+    """Custom error for service unavailable errors (503)."""
+
+    status_code = http.HTTPStatus.SERVICE_UNAVAILABLE
+
+
+class GatewayTimeoutError(CustomError):
+    """Custom error for gateway timeout errors (504)."""
+
+    status_code = http.HTTPStatus.GATEWAY_TIMEOUT
